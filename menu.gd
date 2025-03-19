29556af960946b9c5be25ec27babd4e012336316
PoppinPaulie <paulroos954@hotmail.com>
2025-03-19 19:46:41 -0400

    hi

---
+++ menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+#Add scene change 
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit() 
