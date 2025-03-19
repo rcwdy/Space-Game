dbfbf8a0d74e096317eed7336721f389365635de
rcwdy <rowdyrolls@yahoo.com>
2025-03-19 17:41:26 -0400

    Alpha 0.011

--- entities/boulder_spawner.gd
+++ entities/boulder_spawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# Child of world, spawns boulders
+
 var boulder = preload("res://entities/boulder.tscn")
 
 func _ready() -> void:
@@ -20,3 +22,15 @@ func createBoulder(instr : int):
 		var boulder_make = boulder.instantiate()
 		add_child(boulder_make,1)
 		#print("Boulder Created @ " + str(boulder_make.position))
+
+## TO-DO
+func createBoulder2():
+	if(get_child_count() < 16):
+		var boulder_make = boulder.instantiate()
+		var side = randi_range(0,3)
+		match(side):
+			0:
+				# Spawning on top
+				boulder_make.global_position = Vector2(randf_range(0,DisplayServer.window_get_size().x),0)
+				boulder_make.direction = randf_range(180,360)
+		add_child(boulder_make,1)	
