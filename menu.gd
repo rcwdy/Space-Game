1071b3126de2f7d44735800236e8bfb44ae7714d
PoppinPaulie <paulroos954@hotmail.com>
2025-03-24 16:54:31 -0400

    Menu Changes

--- menu.gd
+++ menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/StartButton.grab_focus()
+	pass
 #Add scene change 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_quit_button_pressed() -> void:
