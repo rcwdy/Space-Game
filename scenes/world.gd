82a775c7e06709b868f3e9c61cf365fe124f3a59
rcwdy <rowdyrolls@yahoo.com>
2025-03-26 08:56:00 -0400

    Fix for "Missing Project" Error

--- scenes/world.gd
+++ scenes/world.gd
@@ -3,6 +3,6 @@ var time = 0
 
 func _process(_delta: float) -> void:
 	$"Health Placeholder".text = "Health:" +str(Globals.player_health)
-	time += delta
+	time += _delta
 	$"Timer Placeholder".text = str(int(time))
 	$"Score Placeholder".text = "Score:" + str(Globals.player_score)
