7496e892ed69ff12e19e71db85a48db7f6cb42e5
rcwdy <rowdyrolls@yahoo.com>
2025-04-02 13:49:54 -0400

    Fixed Music from playing very loud.

--- settings.gd
+++ settings.gd
@@ -2,7 +2,8 @@ extends Control
 
 
 func _on_volume_value_changed(value: float):
-	AudioServer.set_bus_volume_db(0, value)
+	$AudioStreamPlayer.set_volume_db(value)
+	#AudioServer.set_bus_volume_db(0, value)
 
 
 func _on_mute_toggled(toggled_on: bool) -> void:
