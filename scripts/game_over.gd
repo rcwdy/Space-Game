bb7790739ea3273d67594af1907a9ba59e8c288a
isaac11123 <isoriano1223@gmail.com>
2025-04-15 02:11:23 -0400

    implemented submitting username to save score

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,32 +1,6 @@
 extends Control
 
-	if $HttpRequest:
-		$HttpRequest.request_completed.connect(_on_request_completed)
-		send_score("Test User", Globals.player_score)
-	else:
-		print("HttpRequest is null!")
- 
 
-func send_score(player_name, player_score):
-	var url = "http://localhost:3000/submit-score"
-	var headers = ["Content-Type: application/json"]
-	var body = {
-		"player": player_name,
-		"score": player_score
-	}
-	var json_body = JSON.stringify(body)
-
-	if $HttpRequest:
-		print("Sending Score: %s, %d" % [player_name, player_score])
-		$HttpRequest.request(url, headers, HTTPClient.METHOD_POST, json_body)
-	else:
-		print("HttpRequest is null")
-
-func _on_request_completed(result, response_code, headers, body):
-	if response_code == 201:
-		print("Score submitted")
-	else:
-		print("Failed to submit score:", response_code)
 
 func _on_quit_button_pressed() -> void:
 	Globals.save_game()
--- scripts/score_submission.gd
+++ scripts/score_submission.gd
@@ -1,12 +1,33 @@
 extends Control
 
-var score = str(Globals.player_score)
+@onready var http_request = $HttpRequest
+@onready var name_input = $PanelContainer/VBoxContainer/LineEdit
+@onready var score_label = $PanelContainer/VBoxContainer/Score
 
 func _ready():
 	$AnimationPlayer.play("RESET")
 	$AnimationPlayer.play("blur")
-	$PanelContainer/VBoxContainer/Score.text = "Score: " + score
-
-func _on_line_edit_text_submitted(new_text: String) -> void:
-	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+	score_label.text = "Score: " + str(Globals.player_score)
+	http_request.request_completed.connect(_on_request_completed)
+	name_input.grab_focus()
 	
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	var name = new_text.strip_edges()
+	if name == "":
+		name = "Anonymous"
+	send_score(name, Globals.player_score)
+
+func send_score(player_name: String, player_score: int) -> void:
+	var url = "http://localhost:3000/submit-score"
+	var headers = ["Content-Type: application/json"]
+	var body = {
+		"player": player_name,
+		"score": player_score
+	}
+	var json_body = JSON.stringify(body)
+
+	http_request.request(url, headers, HTTPClient.METHOD_POST, json_body)
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code == 201:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
